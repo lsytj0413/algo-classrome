8b471d8e9b41f3fed0558a64b65e3daaef88d679
lsytj0413 <511121939@qq.com>
2018-11-04 21:35:20 +0800

    refactor(go): p0307 range sum query mutable, 树状数组解法

--- go/p0307/p0307.go
+++ go/p0307/p0307.go
@@ -14,28 +14,78 @@
 
 package p0307
 
+// type NumArray struct {
+// 	arr []int
+// }
+
+// func Constructor(nums []int) NumArray {
+// 	arr := make([]int, len(nums))
+// 	copy(arr, nums)
+// 	return NumArray{
+// 		arr: arr,
+// 	}
+// }
+
+// func (this *NumArray) Update(i int, val int) {
+// 	this.arr[i] = val
+// }
+
+// func (this *NumArray) SumRange(i int, j int) int {
+// 	sum := 0
+// 	for ; i <= j; i++ {
+// 		sum += this.arr[i]
+// 	}
+// 	return sum
+// }
+
+// DOC: 树状数组解法
 type NumArray struct {
-	arr []int
+	arr  []int
+	sums []int
+}
+
+func lowbit(x int) int {
+	return x & (0 - x)
+}
+
+func change(i int, val int, arr []int) {
+	for i < len(arr) {
+		arr[i] += val
+		i += lowbit(i)
+	}
+}
+
+func sum(n int, arr []int) int {
+	sum := 0
+	for n > 0 {
+		sum += arr[n]
+		n -= lowbit(n)
+	}
+	return sum
 }
 
 func Constructor(nums []int) NumArray {
 	arr := make([]int, len(nums))
 	copy(arr, nums)
+
+	sums := make([]int, len(nums)+1)
+	for i := 0; i < len(nums); i++ {
+		change(i+1, nums[i], sums)
+	}
 	return NumArray{
-		arr: arr,
+		arr:  arr,
+		sums: sums,
 	}
 }
 
 func (this *NumArray) Update(i int, val int) {
+	delta := val - this.arr[i]
 	this.arr[i] = val
+	change(i+1, delta, this.sums)
 }
 
 func (this *NumArray) SumRange(i int, j int) int {
-	sum := 0
-	for ; i <= j; i++ {
-		sum += this.arr[i]
-	}
-	return sum
+	return sum(j+1, this.sums) - sum(i, this.sums)
 }
 
 /**

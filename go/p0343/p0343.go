c0631232b903e59730902cdef9f81e5c007780ad
lsytj0413 <511121939@qq.com>
2018-11-07 17:53:16 +0800

    feat(go): p0343 integer break

---
+++ go/p0343/p0343.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2018 soren yang
+//
+// Licensed under the MIT License
+// you may not use this file except in complicance with the License.
+// You may obtain a copy of the License at
+//
+//     https://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package p0343
+
+// TODO: 公式解法
+func integerBreak(n int) int {
+	_f1 := func(n int) int {
+		dp := make([]int, n+1)
+		dp[2] = 1
+
+		for i := 3; i <= n; i++ {
+			max := 0
+			for j := 2; j < i; j++ {
+				if j*(i-j) > max {
+					max = j * (i - j)
+				}
+				if dp[j]*(i-j) > max {
+					max = dp[j] * (i - j)
+				}
+			}
+			dp[i] = max
+		}
+
+		return dp[n]
+	}
+	_ = _f1
+
+	return _f1(n)
+}
